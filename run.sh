4e3ff43443b764c5e23e7eea253415b0168a7199
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-03-22 10:17:25 -0700

    Update URL in run.sh script

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ ln -s /home/pi/Pictures/MasterPicsResize_SPLIT /home/pi/photo-frame-tornado/stat
 rm -f /home/pi/photo-frame-tornado/picinfo.db;
 python3 setup.py;
 python3 photoframe.py;
-DISPLAY=:0 nohup firefox --kiosk http://localhost:8888/
+DISPLAY=:0 nohup firefox --kiosk http://192.168.0.26:8888/
