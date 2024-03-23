e07dcbcf4d5d312a1ded6ae103d5923cfc0591ca
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-03-22 20:57:41 -0700

    Add systemd service files and update run.sh script

--- photoframe.py
+++ photoframe.py
@@ -2,21 +2,115 @@ import os
 import sqlite3
 import tornado.ioloop
 import tornado.web
-from pprint import pprint as printf
-import logging as log
+
+class Setup:
+    def __init__(self):
+        self.global_idx = 0
+        self.global_count = 0
+
+    def set_env_vars(self):
+        os.environ['PFPICPATH'] = '/home/pi/photo-frame-tornado/static/MasterPicsResize_SPLIT/'
+        os.environ['PFDBPATH'] = '/home/pi/photo-frame-tornado/picinfo.db'
+        print('Environment variables set')
+
+    def connect_to_db(self):
+        db_path = os.environ.get('PFDBPATH')
+        conn = sqlite3.connect(db_path)
+        return conn
+    
+    def create_table(self):
+        conn = self.connect_to_db()
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS picinfo (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            pfidx INTEGER NOT NULL UNIQUE,
+            pfpath TEXT NOT NULL UNIQUE,
+            pfhttp TEXT NOT NULL
+            )
+        ''')
+        conn.commit()
+        conn.close()
+        print('Table created')
+
+    def check_create_db(self):
+        db_path = os.environ.get('PFDBPATH')
+        if not os.path.exists(db_path):
+            self.create_table()
+            self.global_count = 0
+        else:
+            try:
+                conn = self.connect_to_db()
+                cursor = conn.cursor()
+                cursor.execute('SELECT MAX(pfidx) FROM picinfo')
+                result = cursor.fetchone()
+                if result[0] is not None:
+                    self.global_count = result[0]
+                else:
+                    self.global_count = 0
+                conn.close()
+            except sqlite3.OperationalError:
+                open(db_path, 'a').close()
+                self.global_count = 0
+            print('Database checked and created')
+
+    def walk_files(self):
+        print("Starting walk_files")
+        flist = []
+        pic_path = os.environ.get('PFPICPATH')
+        print(pic_path)
+        for root, dirs, files in os.walk(pic_path):
+            for file in files:
+                if file.endswith('.jpg'):
+                    print(file)
+                    flist.append(os.path.join(root, file))
+        return flist
+
+    def get_file_info(self, filez):
+        idx = 0
+        for file in filez:
+            idx += 1
+            pfpath = file
+            print(idx)
+            print(pfpath)
+            dir, filet = os.path.split(file)
+            kir, folder = os.path.split(dir)
+            _, folder2 = os.path.split(kir)
+            pfhttp = os.path.join('/static/', folder2, folder, filet)
+            print(pfhttp)
+            data = {
+                'pfidx': idx,
+                'pfpath': pfpath,
+                'pfhttp': pfhttp
+            }
+            conn = self.connect_to_db()
+            cursor = conn.cursor()
+            cursor.execute('''
+                INSERT OR IGNORE INTO picinfo (pfidx, pfpath, pfhttp)
+                VALUES (:pfidx, :pfpath, :pfhttp)
+            ''', data)
+            conn.commit()
+            conn.close()
+
+    def main(self):
+        print('Setting environment variables')
+        self.set_env_vars()
+        print('Checking for and creating the database')
+        self.check_create_db()
+        if self.global_count == 0:
+            print('Creating the table')
+            self.create_table()
+            print('Walking the files')
+            pf_files = self.walk_files()
+            self.get_file_info(pf_files)
+        else:
+            print("db file exists nothting to do")
+
+setup = Setup()
+setup.main()
 
 GLOBAL_IDX = 0
 GLOBAL_COUNT = 0
-
-def set_env_vars():
-    os.environ['PFPICPATH'] = '/home/pi/photo-frame-tornado/static/MasterPicsResize_SPLIT'
-    os.environ['PFDBPATH'] = '/home/pi/photo-frame-tornado/picinfo.db'
-    os.environ['GLOBAL_IDX'] = '0'
-    os.environ['GLOBAL_COUNT'] = '0'
-    print('Environment variables set')
-
-set_env_vars()
-
 
 # Define the handler for the main page
 class MainHandler(tornado.web.RequestHandler):
@@ -33,19 +127,6 @@ class MainHandler(tornado.web.RequestHandler):
         conn.close()
         return str(result[0])
     
-
-    # def get_global_idx(self):
-    #     db_path = os.environ.get('PFDBPATH')
-    #     print(db_path)
-    #     conn = sqlite3.connect(db_path)
-    #     cursor = conn.cursor()
-    #     cursor.execute('SELECT * FROM globals')
-    #     result = cursor.fetchone()
-    #     conn.close()
-    #     IDX = result[0]
-    #     IDX = str(IDX)
-    #     return IDX
-    
     def get_addr(self, IDX):
         print("this is IDX IDX {}".format(IDX))
         db_path = os.environ.get('PFDBPATH')
@@ -59,7 +140,6 @@ class MainHandler(tornado.web.RequestHandler):
         new_idx = str(idx)
         os.environ['GLOBAL_IDX'] = new_idx
         return addr
-    
     
     async def get(self):
         zoo = self.set_global_count()
@@ -86,13 +166,6 @@ class MainHandler(tornado.web.RequestHandler):
             addr = "/static/MasterPicsResize_SPLIT/1/04728da2-0831-48ad-b75b-8b3cab8f9269.jpg"
             # Render the template and pass some data to it
             self.render("index.html", title="Photo Frame", addr=addr)
-
-        
-            
-
-
-
-
 
 # Configure the application settings
 settings = {
--- run.sh
+++ run.sh
@@ -1,5 +1,23 @@
-ln -s /home/pi/Pictures/MasterPicsResize_SPLIT /home/pi/photo-frame-tornado/static/;
-rm -f /home/pi/photo-frame-tornado/picinfo.db;
-python3 setup.py;
-python3 photoframe.py;
-DISPLAY=:0 nohup firefox --kiosk http://192.168.0.26:8888/
+if [ ! -L /home/pi/photo-frame-tornado/static/MasterPicsResize_SPLIT ]; then
+    ln -s /home/pi/Pictures/MasterPicsResize_SPLIT /home/pi/photo-frame-tornado/static/;
+fi
+
+# rm -f /home/pi/photo-frame-tornado/picinfo.db;
+
+if [ ! -f /etc/systemd/system/photoframeserver.service ]; then
+    sudo cp /home/pi/photo-frame-tornado/photoframeserver.service /etc/systemd/system/;
+    sudo chmod 644 /etc/systemd/system/photoframeserver.service;
+    sudo chown root:root /etc/systemd/system/photoframeserver.service;
+    sudo systemctl daemon-reload;
+    sudo systemctl enable photoframeserver.service;
+    sudo systemctl start photoframeserver.service;
+fi
+
+if [ ! -f /etc/systemd/system/photoframedisplay.service ]; then
+    sudo cp /home/pi/photo-frame-tornado/photoframedisplay.service /etc/systemd/system/;
+    sudo chmod 644 /etc/systemd/system/photoframedisplay.service;
+    sudo chown root:root /etc/systemd/system/photoframedisplay.service;
+    sudo systemctl daemon-reload;
+    sudo systemctl enable photoframedisplay.service;
+    sudo systemctl start photoframedisplay.service;
+fi
