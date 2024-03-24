28e5a4b4641db5b09e1036241d78c8dcfd3d9492
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-03-24 11:37:16 -0700

    Update run.sh script to pull changes from remote repository

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ fi
 
 if [ -d /usr/share/photo-frame-tornado/photo-frame-tornado ]; then
     rm -rf /usr/share/photo-frame-tornado/photo-frame-tornado/static;
-    cd /usr/share/photo-frame-tornado;
-    git clone https://github.com/cjsmocjsmo/photo-frame-tornado.git
+    cd /usr/share/photo-frame-tornado/photo-frame-tornado;
+    git pull https://github.com/cjsmocjsmo/photo-frame-tornado.git
     cd;
 fi
 
@@ -33,7 +33,7 @@ fi
 
 if [ ! -f /etc/systemd/system/photoframedisplay.service ]; then
     sudo cp /usr/share/photo-frame-tornado/photo-frame-tornado/photoframedisplay.service /etc/systemd/system/;
-    sudo chmod 644 /etc/systemd/system/photoframedisplay.service;
+    sudo chmod 755 /etc/systemd/system/photoframedisplay.service;
     sudo chown root:root /etc/systemd/system/photoframedisplay.service;
     sudo systemctl daemon-reload;
     sudo systemctl enable photoframedisplay.service;
